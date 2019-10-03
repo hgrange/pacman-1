fae3b10b97d410d6acf6686fec19961954294b4d
gestrem <gestrem@redhat.com>
2019-10-03 11:19:27 +0200

    add mongodb svc

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
-
+/*
 Database.connect(app, function(err) {
     if (err) {
         console.log('Failed to connect to database server');
@@ -55,5 +55,5 @@ Database.connect(app, function(err) {
     }
 
 });
-
+*/
 module.exports = app;
