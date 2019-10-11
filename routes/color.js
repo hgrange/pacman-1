21fe989b8979d6f749d3c3e3be974ed471efe3f8
gestrem <gestrem@redhat.com>
2019-10-11 14:52:20 +0200

    black

--- routes/color.js
+++ routes/color.js
@@ -16,7 +16,7 @@ router.use(function timeLog (req, res, next) {
 
 router.get('/colors',urlencodedParser,function(req,res,next) {
        
-        var color = 'blue';
+        var color = 'black';
         console.log("called color : "+color);
         //res.status(200);
         res.json(color);
