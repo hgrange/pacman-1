b46b269d4301840f2cdf5a4a2d3a63fdf28feed1
Philippe HUET <phuet@redhat.com>
2019-12-03 13:27:00 +0100

    Switch color to redhat

--- routes/color.js
+++ routes/color.js
@@ -16,9 +16,9 @@ router.use(function timeLog (req, res, next) {
 
 router.get('/colors',urlencodedParser,function(req,res,next) {
        
-        var color = 'rgb(0, 0, 0)';
+        // var color = 'rgb(0, 0, 0)';
         // Red Hat
-        // var color = 'rgb(197, 33, 33)';
+        var color = 'rgb(197, 33, 33)';
         // IBM
         // var color = 'rgb(59, 108, 170)';
         console.log("called color : "+color);
